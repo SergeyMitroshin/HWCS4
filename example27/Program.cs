914a5103dfd71974906bef8e3ca1ad23898c7c6c
SergeyMitroshin <sergey@host-best.com>
2022-11-25 01:46:26 +0300

    v02

---
+++ example27/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int summa (string arg)
+{
+    int result =0;
+    for (int i=0; i<arg.Length; i++)
+        result = result + int.Parse(arg.Substring (i,1));
+    return result;
+}
+
+Console.WriteLine("введите число");
+string  str =  Console.ReadLine();
+Console.WriteLine(summa(str));
